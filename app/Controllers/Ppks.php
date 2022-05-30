87ac3aa187aa72c2857f030bac06d34962e17a9f
lucidlynk <buset.iwan@gmail.com>
2022-05-31 00:24:11 +0700

    Update Ppks.php

--- app/Controllers/Ppks.php
+++ app/Controllers/Ppks.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Controllers;
-
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 use App\Models\PmksModel;
+use Kint\Renderer\Renderer;
 
 class Ppks extends BaseController
 {
@@ -109,6 +111,47 @@ class Ppks extends BaseController
     }
 
 
+    // create function to export database to csv
+    public function export_excel()
+    {
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'No');
+        $sheet->setCellValue('B1', 'Noka');
+        $sheet->setCellValue('C1', 'Kk');
+        $sheet->setCellValue('D1', 'Nik');
+        $sheet->setCellValue('E1', 'Nama');
+        $sheet->setCellValue('F1', 'Pisat');
+        $sheet->setCellValue('G1', 'Tmp_lahir');
+        $sheet->setCellValue('H1', 'Tgl_lahir');
+        $sheet->setCellValue('I1', 'Jk');
+        $sheet->setCellValue('J1', 'Status');
+        $sheet->setCellValue('K1', 'Alamat');
+        $sheet->setCellValue('L1', 'Kd_pos');
+        $sheet->setCellValue('M1', 'Kecamatan');
+        $sheet->setCellValue('N1', 'Desa');
 
-    
+        $sheet->getStyle('A1:N1')->getFont()->setBold(true);
+
+        $data = $this->usulkisModel->findAll();
+        $no = 1;
+        $i = 2;
+        foreach ($data as $d) {
+            $sheet->setCellValue('A' . $i, $no);
+            $sheet->setCellValue('B' . $i, $d['noka']);
+            $sheet->setCellValue('C' . $i, $d['kk']);
+            $sheet->setCellValue('D' . $i, $d['nik']);
+            $sheet->setCellValue('E' . $i, $d['nama']);
+            $sheet->setCellValue('F' . $i, $d['pisat']);
+            $sheet->setCellValue('G' . $i, $d['tmp_lahir']);
+            $i++;
+        }
+        $writer = new Xlsx($spreadsheet);
+        $filename = 'Data_usul_kis_'.date('Y-m-d_H-i-s').'.xlsx';
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="'. $filename .'"');
+        header('Cache-Control: max-age=0');
+        $writer->save('php://output');
+        redirect()->to('kis/input');
+    }
 }
