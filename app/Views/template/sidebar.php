87351353a244257df86534c2c8ff3e76e6d75cdc
lucidlynk <buset.iwan@gmail.com>
2022-06-05 22:03:57 +0700

    05062022 10:03 PM

--- app/Views/template/sidebar.php
+++ app/Views/template/sidebar.php
@@ -105,11 +105,15 @@
             <!-- Divider -->
             <hr class="sidebar-divider">
             <!-- Logout -->
-            <li class="nav-item">
+            <!-- <li class="nav-item">
                 <a class="nav-link" href="/logout">
                     <i class="fas fa-sign-out-alt"></i>
                     <span>Logout</span></a>
-            </li>
+            </li> -->
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
             
 
         </ul>
